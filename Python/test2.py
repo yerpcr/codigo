080a79fd008917c1f72676100bf0b3eba194022b
Yordan Rodriguez <yerp35@gmail.com>
2019-07-28 15:22:00 -0500

    update

--- Python/test2.py
+++ Python/test2.py
@@ -2,7 +2,7 @@ import time
 import serial 
 import os
 
-file = open("./doc/dia1.txt","w")
+file = open ("./doc/dia1.txt","w")
 
 ser = serial.Serial('/dev/ttyUSB0',9600)
 a = time.time()
@@ -10,6 +10,7 @@ while 1 :
     s = ser.readline()
     if s!='Hola':
         file.write(s+"\t"+str(time.time())+"\n")
+        print(s+"\n")
     else: 
         print (s)
 
