3f2b325d0f673fb6e7f7e6bc254b785232a1ae7c
Yordan Rodriguez <yerp35@gmail.com>
2019-08-19 04:20:01 +0000

    ultimos cambios

--- Python/test3.py
+++ Python/test3.py
@@ -2,13 +2,33 @@ import time
 import serial 
 import os
 
-print("\n Hi, master. Could you help me with the file's name:\n")
+def cadena (entrada):
+    i=1
+    salida =""
+    for x in range (0,len(entrada)):
+        if (entrada[x]==","):
+            i=i+1
+            salida += "\t"
+        else:
+            salida += entrada[x]
+    return (i,salida)
+
+print("\n Hola, maestro. Podrias ayudarme con el nombre del archivo:\n")
 nombre = input()
 archivo = "./doc/"+str(nombre)
 file = open (archivo,"w")
 print("\nFiles " + str(nombre) + " created")
-
+while (True):
+    variables = input("Digita el nombre de las veriables. Recuerda separar cada una de ellas con una coma:\n")
+    n,o = cadena(variables)
+    if (n>5):
+        print("Recuerde que es un maximo de 5 analogos. Vuelvalo a intentar.\n")
+    else:
+        break
+print ("Son "+str(n)+" variables:\n")
+file.write(o)
 ser = serial.Serial('/dev/ttyO2',9600)
+ser.write(n)
 a = time.time()
 while 1 :
     s = ser.readline()
