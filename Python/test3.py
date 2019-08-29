a08c2f328f2fe5b827f1d0e09c77123d3ce4d5ca
Yordan Rodriguez <yerp35@gmail.com>
2019-08-29 12:00:15 -0500

    Ultimos cambios

--- Python/test3.py
+++ Python/test3.py
@@ -1,7 +1,63 @@
 import time 
 import serial 
 import os
-nulo=b''
+
+
+def main ():
+    
+    archivo1 = crearArchivo()
+    n_variables = Variables (archivo1)
+
+    arduino = serial.Serial('COM3',9600,timeout=0.5)
+    
+    print("Serial configurado\n")
+    
+    envioVariables(n_variables,arduino)
+
+    print ("Inicio de datos")
+    a = time.time()
+    ent=""
+    while True :
+        ent=entradaDatos(arduino,ent,archivo1)
+
+def crearArchivo():
+    print("\n Hola, maestro. Podrias ayudarme con el nombre del archivo:\n")
+    nombre = input()
+    archivo = "./doc/"+str(nombre)
+    file = open (archivo,"w")
+    print("\nArchivo " + str(nombre) + " creado")    
+    return(file)
+
+def Variables(file):
+    while (True):
+        variables = input("Digita el nombre de las veriables. Recuerda separar cada una de ellas con una coma:\n")
+        n,o = cadena(variables)
+        if (n>6):
+            print("Recuerde que es un maximo de 6 analogos. Vuelvalo a intentar.\n")
+        else:
+            break
+
+    ##fin bucle
+
+    o+="\n"
+    print ("Son "+str(n)+" variables: ")
+    file.write(o)
+    return(n)
+
+def envioVariables(n,ser):
+    nulo = b''
+    test = 0
+    while(test!=n):
+        ser.write(str.encode(str(n)))
+        while (ser.inWaiting()):
+            time.sleep(0.01)
+        q=ser.readline()
+        print (q)
+        if (q!=nulo):
+            test = int(q)
+            print(test)
+            ser.flush()
+
 def cadena (entrada):
     i=1
     salida =""
@@ -13,59 +69,17 @@ def cadena (entrada):
             salida += entrada[x]
     return (i,salida)
 
-
-print("\n Hola, maestro. Podrias ayudarme con el nombre del archivo:\n")
-nombre = input()
-archivo = "./doc/"+str(nombre)
-file = open (archivo,"w")
-print("\nFiles " + str(nombre) + " created")
-
-##bucle numero de analogos
-
-while (True):
-    variables = input("Digita el nombre de las veriables. Recuerda separar cada una de ellas con una coma:\n")
-    n,o = cadena(variables)
-    if (n>5):
-        print("Recuerde que es un maximo de 5 analogos. Vuelvalo a intentar.\n")
-    else:
-        break
-
-##fin bucle
-
-o+="\n"
-print ("Son "+str(n)+" variables: ")
-file.write(o)
-ser = serial.Serial('/dev/ttyUSB0',9600,timeout=0.5)
-test=0
-print("Serial configurado\n")
-
-
-##check
-while(test!=n):
-    ser.write(str.encode(str(n)))
-    while (ser.inWaiting()):
-        time.sleep(0.01)
-    q=ser.readline()
-    print (q)
-    if (q!=nulo):
-        test = int(q)
-        print(test)
-        ser.flush()
-
-##fin check
-print ("Inicio de datos")
-
-a = time.time()
-ent=""
-print("ok\n")
-while 1 :
+def entradaDatos(ser,entrada,file):
     s = ser.readline()
-    if (s==b'*'):
-        ent+="\n"
-        print (ent)
-        file.write(ent)
-        ent=""
+    if (s==b'*\r\n'):
+        entrada+="\n"
+        print (entrada)
+        file.write(entrada)
+        entrada=""
     else:
-        ent += "\t"
-        ent += str(int(s))
+        entrada += "\t"
+        entrada += str(int(s))
         print (s)
+    return(entrada)
+
+main ()
--- datos/datos.ino
+++ datos/datos.ino
@@ -1,24 +1,44 @@
-const int sensor = A0;
+String indata="",read_String="";
 const int n = 500;
-int i=0,j=0;
+int i=0,j=0,k=0;
 int x[]={0,0,0,0,0};
 float m=0;
-int n_analogos=0,espera=500,listo=1;
+int n_analogos=0,q,espera=500,listo=1;
 int led = 13;
+char c;
 void setup(){
   pinMode(led,OUTPUT);
   digitalWrite(led,0);
 	Serial.begin(9600); 
   while(listo){
-	if (Serial.available()>0){
-	n_analogos = Serial.read();
-	Serial.print(n_analogos);
-	}
- if( n_analogos>48 && n_analogos<55){
-    n_analogos=n_analogos-48;
-    listo=0;
-    digitalWrite(led,1);
-  }
+    read_String="";
+    while (!Serial.available()) {}
+	  digitalWrite(led,1);
+	  while (Serial.available()) // this will be skipped if no data present, leading to
+                             // the code sitting in the delay function below
+    {
+      delay(30);  //delay to allow buffer to fill 
+      if (Serial.available() >0)
+      {
+        c = Serial.read();  //gets one byte from serial buffer
+        read_String+= c;
+         c=0;
+      }
+    }
+    digitalWrite(led,0);
+    Serial.println(read_String);
+    if (read_String=="1")n_analogos=1;
+    if (read_String=="2")n_analogos=2;
+    if (read_String=="3")n_analogos=3;
+    if (read_String=="4")n_analogos=4;
+    if (read_String=="5")n_analogos=5;
+    if (read_String=="6")n_analogos=6;
+    if(n_analogos>0&&n_analogos<7){
+    
+      listo=0;
+      digitalWrite(led,1);
+      
+    }
   }
 }
 void loop(){
@@ -45,8 +65,8 @@ void loop(){
 		}
 	}
 	for (i=0; i<n_analogos; i++){
-		Serial.print(x[i]);
+		Serial.println(x[i]);
 	}
-	Serial.print("*");
+	Serial.println("*");
 	delay(espera);
 }
